a273da5132e6ec0dc021a4c9727597120434587d
Xavier189 <2593331363@qq.com>
2022-08-24 09:44:20 +0800

    读取计价规则--xuwei

--- service-price/src/main/java/com/mashibing/serviceprice/service/ForecastPriceService.java
+++ service-price/src/main/java/com/mashibing/serviceprice/service/ForecastPriceService.java
@@ -1,14 +1,22 @@
 package com.mashibing.serviceprice.service;
 
 
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
+import com.mashibing.internalcommon.dto.PriceRule;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.ForecastPriceDTO;
 import com.mashibing.internalcommon.responese.DirectionResponse;
 import com.mashibing.internalcommon.responese.ForecastPriceResponse;
+import com.mashibing.serviceprice.mapper.PriceRuleMapper;
 import com.mashibing.serviceprice.remote.ServiceMapClient;
+import com.sun.javafx.collections.MappingChange;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -17,6 +25,9 @@ public class ForecastPriceService {
 
     @Autowired
     private ServiceMapClient serviceMapClient;
+
+    @Autowired
+    private PriceRuleMapper priceRuleMapper;
 
     public ResponseResult forecastPrice(String depLongitude,String depLatitude,String destLongitude,String destLatitude){
 
@@ -36,7 +47,18 @@ public class ForecastPriceService {
         Integer distance = direction.getData().getDistance();
         Integer duration = direction.getData().getDuration();
         log.info("距离：" + distance + "，时长：" + duration);
+
         log.info("读取计价规则");
+        Map<String,Object> queryMap = new HashMap<>();
+        queryMap.put("city_code","110000");
+        queryMap.put("vehicle_type","1");
+        List<PriceRule> priceRules = priceRuleMapper.selectByMap(queryMap);
+        if (priceRules.size() == 0){
+            return ResponseResult.fail(CommonStatusEnum.PRICE_RULE_EMPTY.getCode(),CommonStatusEnum.PRICE_RULE_EMPTY.getValue());
+
+        }
+
+        PriceRule priceRule = priceRules.get(0);
 
         log.info("根据距离、时长和计价规则，计算价格");
 
