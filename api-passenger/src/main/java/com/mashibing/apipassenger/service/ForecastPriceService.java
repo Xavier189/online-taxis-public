33c8ff40a056952a657e46ffc6a85078d8592ce6
Xavier189 <2593331363@qq.com>
2022-08-24 11:38:19 +0800

    优化计价规则代码--xuwei

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/ForecastPriceService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/ForecastPriceService.java
@@ -1,15 +1,20 @@
 package com.mashibing.apipassenger.service;
 
 
+import com.mashibing.apipassenger.remote.ServicePriceClient;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.request.ForecastPriceDTO;
 import com.mashibing.internalcommon.responese.ForecastPriceResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ForecastPriceService {
 
+    @Autowired
+    private ServicePriceClient servicePriceClient;
 
     /**
      * 根据出发地和目的地的经纬度 计算出预估价格 *
@@ -27,10 +32,17 @@ public class ForecastPriceService {
         log.info("目的地纬度：" + destLatitude);
 
         log.info("调用计价服务");
+        ForecastPriceDTO forecastPriceDTO = new ForecastPriceDTO();
+        forecastPriceDTO.setDestLongitude(destLongitude);
+        forecastPriceDTO.setDestLatitude(destLatitude);
+        forecastPriceDTO.setDepLongitude(depLongitude);
+        forecastPriceDTO.setDepLatitude(depLatitude);
+        ResponseResult<ForecastPriceResponse> responseResult = servicePriceClient.forecastPrice(forecastPriceDTO);
+        double price = responseResult.getData().getPrice();
 
         ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
 
-        forecastPriceResponse.setPrice(12.3);
+        forecastPriceResponse.setPrice(price);
 
         return ResponseResult.success(forecastPriceResponse);
 
--- service-price/src/main/java/com/mashibing/serviceprice/service/ForecastPriceService.java
+++ service-price/src/main/java/com/mashibing/serviceprice/service/ForecastPriceService.java
@@ -7,6 +7,7 @@ import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.ForecastPriceDTO;
 import com.mashibing.internalcommon.responese.DirectionResponse;
 import com.mashibing.internalcommon.responese.ForecastPriceResponse;
+import com.mashibing.internalcommon.utils.BigDecimalUtils;
 import com.mashibing.serviceprice.mapper.PriceRuleMapper;
 import com.mashibing.serviceprice.remote.ServiceMapClient;
 import com.sun.javafx.collections.MappingChange;
@@ -14,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -61,14 +63,84 @@ public class ForecastPriceService {
         PriceRule priceRule = priceRules.get(0);
 
         log.info("根据距离、时长和计价规则，计算价格");
+        double price = getPrice(distance, duration, priceRule);
+
 
         ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
 
-        forecastPriceResponse.setPrice(12.3);
+        forecastPriceResponse.setPrice(price);
 
         return ResponseResult.success(forecastPriceResponse);
 
     }
+
+    /**
+     *  根据距离、时长和计价规则，计算最终价格 *
+     * @param distance
+     * @param duration
+     * @param priceRule
+     * @return
+     */
+    private double getPrice(Integer distance,Integer duration,PriceRule priceRule ){
+//        BigDecimal price = new BigDecimal(0);
+        double price = 0;
+        // 起步价
+        Double startFare = priceRule.getStartFare();
+
+        price = BigDecimalUtils.add(startFare,price);
+//        BigDecimal startFarePrice = new BigDecimal(startFare);
+//        price = price.add(startFarePrice);
+
+        // 里程费
+        // 总里程 m
+        // 总里程 km
+        double distanceMile = BigDecimalUtils.divide(distance,1000);
+//        BigDecimal distanceMileDecimal = distanceDecimal.divide(new BigDecimal(1000),2,BigDecimal.ROUND_HALF_UP);
+        // 起步里程
+        double startMile = (double) priceRule.getStartMile();
+        double distanceSubtract = BigDecimalUtils.substaact(distanceMile,startMile);
+//        BigDecimal startMileDecimal = new BigDecimal(startMile);
+//        double distanceSubtract = distanceMileDecimal.subtract(startMileDecimal).doubleValue();
+        // 最终收费里程
+        double mile = distanceSubtract<0?0:distanceSubtract;// 如果小于0就取0
+//        BigDecimal mileDecimal = new BigDecimal(mile);
+        // 计程单价
+        double unitPricePerMile = priceRule.getUnitPricePerMile();
+//        BigDecimal unitPriceDecimal = new BigDecimal(unitPricePerMile);
+        // 里程价格
+        double mileFare = BigDecimalUtils.multiply(mile, unitPricePerMile);
+//        double mileFare = mileDecimal.multiply(unitPriceDecimal).setScale(2,BigDecimal.ROUND_HALF_UP);
+        price = BigDecimalUtils.add(price,mileFare);
+
+        // 时长费
+        // 总时长 s
+//        BigDecimal time = new BigDecimal(duration);
+        // 总时长 m
+        double timeMinute = BigDecimalUtils.divide(duration,60);
+        // 时长计时单价
+        double unitPricePerMinute = priceRule.getUnitPricePerMinute();
+//        BigDecimal unitPriceMinuteDecimal = new BigDecimal(unitPricePerMinute);
+        // 时长价格
+        double timeFare = BigDecimalUtils.multiply(timeMinute,unitPricePerMinute);
+//        BigDecimal timeFare = timeDecimal.multiply(unitPriceMinuteDecimal).setScale(2,BigDecimal.ROUND_HALF_UP);
+        price = BigDecimalUtils.add(price,timeFare);
+
+        // 取两位小数
+        BigDecimal priceDecimal = BigDecimal.valueOf(price);
+        priceDecimal = priceDecimal.setScale(2,BigDecimal.ROUND_HALF_UP);
+
+        return priceDecimal.doubleValue();
+
+    }
+
+//    public static void main(String[] args) {
+//        PriceRule priceRule = new PriceRule();
+//        priceRule.setUnitPricePerMile(1.8);
+//        priceRule.setUnitPricePerMinute(0.5);
+//        priceRule.setStartFare(10.0);
+//        priceRule.setStartMile(3);
+//        System.out.println(getPrice(6500,1800,priceRule));
+//    }
 
 
 }
