3daf00cf12b0fdac29cc88533a832d8335ccd9a6
xiaowei <xavierxuwei@163.com>
2022-12-04 16:02:17 +0800

    update gitignore

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/dto/DriverCarBindingRelations.java
@@ -0,0 +1,29 @@
+package com.mashibing.internalcommon.dto;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class DriverCarBindingRelations {
+    private Long id;
+
+    private Long driverId;
+
+    private Long carId;
+
+    /**
+    * 绑定，1：绑定 2：解绑
+    */
+    private Integer bingState;
+
+    /**
+    * 绑定时间
+    */
+    private LocalDateTime bindingTime;
+
+    /**
+    * 解绑时间
+    */
+    private LocalDateTime unBindingTime;
+}
